f3b82fb02bce40ec58d4a46044c55abc1e76e74b
tuncayaltunn <taltun0855@gmail.com>
2025-01-01 23:25:44 +0300

    Set ConnectionString and Migrations

--- Infrastructure/ETicaretAPI.Persistence/ServiceRegisration.cs
+++ Infrastructure/ETicaretAPI.Persistence/ServiceRegisration.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using ETicaretAPI.Application.Abstractions;
 using ETicaretAPI.Persistence.Concretes;
+using ETicaretAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ETicaretAPI.Persistence
@@ -10,6 +12,10 @@ namespace ETicaretAPI.Persistence
 		public static void AddPersistenceServices(this IServiceCollection services)
 		{
 			services.AddSingleton<IProductService, ProductService>();
+
+			services.AddDbContext<ETicaretAPIDbContext>(options =>
+				options.UseNpgsql(Configuration.ConnectionString)
+			);
 		} 
 	}
 }
