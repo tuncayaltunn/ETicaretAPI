c38ab95179fbe20abff50725a52e94be5ef6524f
tuncayaltunn <taltun0855@gmail.com>
2025-01-01 16:38:07 +0300

    Dummy Data

--- Infrastructure/ETicaretAPI.Persistence/Concretes/ProductService.cs
+++ Infrastructure/ETicaretAPI.Persistence/Concretes/ProductService.cs
@@ -11,8 +11,7 @@ namespace ETicaretAPI.Persistence.Concretes
             new() { Id = Guid.NewGuid(), Name = "Product 1", Price = 100, Stock = 10 },
             new() { Id = Guid.NewGuid(), Name = "Product 2", Price = 200, Stock = 10 },
             new() { Id = Guid.NewGuid(), Name = "Product 3", Price = 300, Stock = 10 },
-            new() { Id = Guid.NewGuid(), Name = "Product 4", Price = 400, Stock = 10 },
-            new() { Id = Guid.NewGuid(), Name = "Product 5", Price = 500, Stock = 10 }
+            new() { Id = Guid.NewGuid(), Name = "Product 4", Price = 400, Stock = 10 }
         };
     }
 }
