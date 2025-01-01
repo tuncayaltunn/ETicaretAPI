33cd81673ef46482217cf83e1eed307641c3a3c2
taft <altun.tuncay@outlook.com>
2025-01-01 16:28:20 +0300

    first commit

---
+++ Core/ETicaretAPI.Application/Abstractions/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using ETicaretAPI.Domain.Entities;
+
+namespace ETicaretAPI.Application.Abstractions
+{
+	public interface IProductService
+	{
+		public List<Product> GetAll();
+	}
+}
+
---
+++ Core/ETicaretAPI.Domain/Entities/Common/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ETicaretAPI.Domain.Entities.Common
+{
+	public class BaseEntity
+	{
+		public Guid Id { get; set; }
+		public DateTime CreatedDate { get; set; }
+	}
+}
+
