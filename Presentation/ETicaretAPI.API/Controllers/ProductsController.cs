1200b2414e6899be129b2536f895e31eb9bc80d1
tuncayaltunn <taltun0855@gmail.com>
2025-01-05 23:56:12 +0300

    Products Api Controller

---
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ETicaretAPI.Application.Repositories.IProductRepositories;
+using ETicaretAPI.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretAPI.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductsController : ControllerBase
+	{
+		private readonly IProductReadRepository _productReadRepository;
+		private readonly IProductWriteRepository _productWriteRepository;
+
+        public ProductsController(IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository)
+        {
+            _productWriteRepository = productWriteRepository;
+            _productReadRepository = productReadRepository;
+        }
+
+        [HttpGet("{id}")]
+		public async Task<IActionResult> Get(string id)
+		{
+			Product product = await _productReadRepository.GetByIdAsync(id);
+
+			return Ok(product);
+		}
+	}
+}
+
