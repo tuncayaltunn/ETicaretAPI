fdb1977c83088688dd7519173b41f54eae08c78c
tuncayaltunn <taltun0855@gmail.com>
2025-01-25 14:11:37 +0300

    File Name Edit

---
+++ Infrastructure/ETicaretAPI.Infrastructure/Operations/NameOperation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ETicaretAPI.Infrastructure.Operations
+{
+	public static class NameOperation
+	{
+		public static string CharacterRegulatory(string name)
+		{
+			name.Replace("ç", "c")
+				.Replace("ğ", "g")
+				.Replace("ı", "i")
+				.Replace("ö", "o")
+				.Replace("ş", "s")
+				.Replace("ü", "u");
+			return name;
+		}
+	}
+}
+
