9decb770cbbb9bae740ba134e1d2deda6427875f
tuncayaltunn <taltun0855@gmail.com>
2025-02-22 23:58:51 +0300

    Global Exception Handler

---
+++ Presentation/ETicaretAPI.API/Extensions/ConfigureExceptionHandlerExtension.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Net.Mime;
+using System.Text.Json;
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace ETicaretAPI.API.Extensions
+{
+	public static class ConfigureExceptionHandlerExtension
+	{
+		public static void ConfigureExceptionHandler<T>(this WebApplication application
+													,ILogger<T> logger)
+		{
+			application.UseExceptionHandler(builder =>
+			{
+				builder.Run(async context =>
+				{
+					context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+					context.Response.ContentType = MediaTypeNames.Application.Json;
+
+					var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+					if (contextFeature != null)
+					{
+						logger.LogError(contextFeature.Error.Message);
+
+						await context.Response.WriteAsync(JsonSerializer.Serialize(new
+						{
+							StatusCode = context.Response.StatusCode,
+							Message = contextFeature.Error.Message,
+							Title = "Hata alındı!"
+						}));
+					}
+				});
+			});
+		}
+	}
+}
+
--- Presentation/ETicaretAPI.API/Program.cs
+++ Presentation/ETicaretAPI.API/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Security.Claims;
 using System.Text;
 using ETicaretAPI.API.Configurations.ColumnWriters;
+using ETicaretAPI.API.Extensions;
 using ETicaretAPI.Application;
 using ETicaretAPI.Application.Validators.Products;
 using ETicaretAPI.Infrastructure;
@@ -94,6 +95,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.ConfigureExceptionHandler<Program>(app.Services.GetRequiredService<ILogger<Program>>());
 
 app.UseStaticFiles();
 
